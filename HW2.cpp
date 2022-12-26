f7c843f704ce27d0571a1034b992d2904440fece
brmcbrid <69523108+brmcbrid@users.noreply.github.com>
2022-12-26 12:44:21 -0500

    Create HW2.cpp

---
+++ HW2.cpp
@@ -0,0 +1,21 @@
+// Write a program that will prompt the user for an 
+// integer n that will be tested to determine if it 
+// is a prime number. Use a for loop to repeatedly test the 
+// integer n to see if an i = 2, 3, 4, 5, . . . etc. up to n-1, 
+// is a divisor of the integer. If you find a number that 
+// divides n evenly then you have proven that the number is not prime.
+// When a divisor is found to prove that the integer n is not prime, 
+// then output the smallest number that divides the integer n.  
+// If all possibilities are exhausted and no divisor was found that 
+// evenly divides integer n then the number must be prime.
+// Your program should handle input errors caused by invalid data entered by the user. 
+// The program should also check with the user to see if they wish to test another number. 
+// If yes, then prompt the user for the number to test. If they answer no then the program should terminate. 
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+    return 0;
+}
