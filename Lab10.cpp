a1fb6340bde0994876edf855fe80891d22e40ae3
brmcbrid <69523108+brmcbrid@users.noreply.github.com>
2023-01-02 19:18:32 -0500

    Create Lab10.cpp

---
+++ Lab10.cpp
@@ -0,0 +1,19 @@
+// Write a program that will prompt the user for an 
+// integer n that will be tested to determine if it 
+// is a prime number. Use a for loop to repeatedly test the 
+// integer n to see if an i = 2, 3, 4, 5, . . . etc. up to n-1, 
+// is a divisor of the integer. If you find a number that 
+// divides n evenly then you have proven that the number is not prime.
+// When a divisor is found to prove that the integer n is not prime, 
+// then output the smallest number that divides the integer n.  
+// If all possibilities are exhausted and no divisor was found that 
+// evenly divides integer n then the number must be prime.
+ 
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+    return 0;
+}
